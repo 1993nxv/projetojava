386210f4b431c483200e152a3f35912826a88385
Wellington Delmondes <wellington.apk@gmail.com>
2022-12-02 21:35:00 -0300

    Acrescentada um novo print.

--- pric.java
+++ pric.java
@@ -5,7 +5,8 @@ public class pric {
     public static void main(String[] args){
 
         System.out.println("Apenas minha primeira versão no Github!");
-        
+        System.out.println("Uma alteração para subir nova versão.");
+
     }
 
 }
